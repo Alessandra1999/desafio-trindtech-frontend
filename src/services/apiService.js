5b8e11385d8200c21d6975f1e763cf9f7cefa319
Alessandra Bombardi <ale.brbr@gmail.com>
2024-11-25 16:28:24 -0300

    ajustes para deploy

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -5,24 +5,24 @@ const API_URL = "https://desafio-trindtech-backend-production.up.railway.app";
 
 // Funções para Alunos
 export const getStudents = async () => {
-  const response = await axios.get(`${API_URL}/students`);
+  const response = await axios.get(`${API_URL}/api/students`);
   return response.data;
 };
 
 export const createStudent = async (studentData) => {
-  const response = await axios.post(`${API_URL}/students`, studentData);
+  const response = await axios.post(`${API_URL}/api/students`, studentData);
   return response.data;
 };
 
 export const getStudentById = async (id_student) => {
-  const response = await axios.get(`${API_URL}/students/${id_student}`);
+  const response = await axios.get(`${API_URL}/api/students/${id_student}`);
   return response.data;
 };
 
 export const updateStudent = async (id_student, studentData) => {
   try {
     const response = await axios.put(
-      `${API_URL}/students/${id_student}`,
+      `${API_URL}/api/students/${id_student}`,
       studentData
     );
     return response.data;
@@ -32,12 +32,12 @@ export const updateStudent = async (id_student, studentData) => {
 };
 
 export const deleteStudent = async (id_student) => {
-  await axios.delete(`${API_URL}/students/${id_student}`);
+  await axios.delete(`${API_URL}/api/students/${id_student}`);
 };
 
 // Funções para Cursos
 export const getCourses = async () => {
-  const response = await axios.get(`${API_URL}/courses`);
+  const response = await axios.get(`${API_URL}/api/courses`);
   return response.data;
 };
 
