a60dbc4933f191bc4f4f7649307f53ba51df5183
Alessandra Bombardi <ale.brbr@gmail.com>
2024-11-25 16:07:49 -0300

    "configurações de deploy"

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Define a URL base
-const API_URL = "https://desafio-trindtech.vercel.app/api";
+const API_URL = "desafio-trindtech-backend-production.up.railway.app/api";
 
 // Funções para Alunos
 export const getStudents = async () => {
